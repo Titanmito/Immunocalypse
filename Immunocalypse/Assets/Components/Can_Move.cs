d21bda99f574ec360176eb144b12b87bd1a1d337
arianacarnielli <ariana.carnielli@gmail.com>
2020-10-30 12:22:32 +0100

    création d'une première version du Moviment_System

---
+++ Immunocalypse/Assets/Components/Can_Move.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Can_Move : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+	public float move_speed = 2.5f;
+	public Vector3 spawn_point = new Vector3(-8.5f, 0.0f);
+	public Vector3 target;
+
+}
---
+++ Immunocalypse/Assets/Systems/Movement_System.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class Movement_System : FSystem {
+
+	private Family _MovingGO = FamilyManager.getFamily(new AllOfComponents(typeof(Can_Move)));
+
+	// Constructeur
+	public Movement_System()
+	{
+		foreach (GameObject go in _MovingGO)
+			onGOEnter(go);
+
+		_MovingGO.addEntryCallback(onGOEnter);
+	}
+
+	private void onGOEnter(GameObject go)
+	{
+		go.transform.position = go.GetComponent<Can_Move>().spawn_point;
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount)
+	{
+		foreach (GameObject go in _MovingGO)
+		{
+			Can_Move cm = go.GetComponent<Can_Move>();
+
+			if (cm.target.Equals(go.transform.position))
+			{
+				go.transform.position = new Vector3(-20.0f, -20.0f);
+				GameObjectManager.unbind(go);
+				//Object.Destroy(go);
+
+			}
+			else
+			{
+				go.transform.position = Vector3.MoveTowards(go.transform.position, cm.target, cm.move_speed * Time.deltaTime);
+			}
+		}
+	}
+
+}
