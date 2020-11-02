86409d7c83dad7aa84b6349fe928d065a5b688cf
arianacarnielli <ariana.carnielli@gmail.com>
2020-11-02 13:03:30 +0100

    une prèmiere version du placement interactif d'un tower est fonctionnelle

--- Immunocalypse/Assets/Components/Bank.cs
+++ Immunocalypse/Assets/Components/Bank.cs
@@ -3,4 +3,5 @@
 public class Bank : MonoBehaviour {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
 	public int energy;
+	public bool used = false;
 }
--- Immunocalypse/Assets/Systems/Energy_System.cs
+++ Immunocalypse/Assets/Systems/Energy_System.cs
@@ -6,6 +6,7 @@ public class Energy_System : FSystem {
 	private Family _Spawn = FamilyManager.getFamily(new AllOfComponents(typeof(Spawn)));
 	private Family _Joueur = FamilyManager.getFamily(new AnyOfTags("Player"));
 	private Family _Macrophage = FamilyManager.getFamily(new AnyOfTags("Tower_Macro"));
+	private Family _Inactive = FamilyManager.getFamily(new NoneOfProperties(PropertyMatcher.PROPERTY.ACTIVE_IN_HIERARCHY, PropertyMatcher.PROPERTY.HAS_PARENT));
 
 	private Spawn spawn;
 	private Price price;
@@ -20,11 +21,15 @@ public class Energy_System : FSystem {
 
 	public void Macro_Button(int amount)
 	{
-		if (bank.energy >= price.energy_cost)
+		if (bank.energy >= price.energy_cost && !bank.used)
 		{
 			GameObject go = UnityEngine.Object.Instantiate<GameObject>(spawn.macro_prefab);
+			go.transform.position = new Vector3(20.0f, 20.0f);
 			GameObjectManager.bind(go);
+			go.SetActive(false);
+
 			bank.energy -= price.energy_cost;
+			bank.used = true;
 		}
 	}
 
@@ -35,6 +40,24 @@ public class Energy_System : FSystem {
 		int secs = (int)Math.Floor(spawn.energy_prog);
 		bank.energy += spawn.energy_sec * secs;
 		spawn.energy_prog -= secs;
+
+		if (bank.used)
+        {
+			if (Input.GetMouseButton(0) && _Inactive.First())
+            {
+				Vector3 mousePos = Input.mousePosition;
+				mousePos.z = Camera.main.nearClipPlane;
+				Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+				GameObject go = _Inactive.First();
+				go.SetActive(true);
+				go.transform.position = worldPosition;
+
+				bank.used = false;
+			}
+
+
+        }
 
 	}
 }
