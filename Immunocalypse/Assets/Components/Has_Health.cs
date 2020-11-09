096629a0c57219609b0033f637e0171b7a76fd52
arianacarnielli <ariana.carnielli@gmail.com>
2020-11-09 17:23:40 +0100

    création d'un health bar

--- Immunocalypse/Assets/Components/Has_Health.cs
+++ Immunocalypse/Assets/Components/Has_Health.cs
@@ -3,6 +3,7 @@
 public class Has_Health : MonoBehaviour {
 	// This component is present in entities that have some kind of health bar (Joueur, bacterie and virus now).
 	
-	// Represents the heath of the entity. The idea is that things happen when it gets to zero (either the entity is destroyed or the player loses the level).
+	// Represents the health of the entity. The idea is that things happen when it gets to zero (either the entity is destroyed or the player loses the level).
 	public int health;
+	public int max_health;
 }
--- Immunocalypse/Assets/Systems/Attack_J_System.cs
+++ Immunocalypse/Assets/Systems/Attack_J_System.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using FYFY;
+using System.Collections;
+
 
 public class Attack_J_System : FSystem {
 	// This system manages the attack of enemies to the player. It actualizes the health of the Joueur entity by going thru each enemy and testing if it can attack or not.
 	// For each enemy that can attack it recalculates the health of the Joueur entity and marks the enemy as having attacked. 
 
 	private Family _AttackingGO = FamilyManager.getFamily(new AllOfComponents(typeof(Attack_J), typeof(Can_Move), typeof(Has_Health)));
-
 	private Family _Joueur = FamilyManager.getFamily(new AnyOfTags("Player"));
+	private Family _HealthBar = FamilyManager.getFamily(new AnyOfTags("Health_Bar"));
 
-	private  GameObject joueur;
+	private GameObject joueur;
+	private Slider healthBar;
 
 	//joueur = GameObject.FindWithTag("Player");
 
 	public Attack_J_System()
 	{
 		joueur = _Joueur.First();
+		healthBar = _HealthBar.First().GetComponent<Slider>();
+		healthBar.maxValue = joueur.GetComponent<Has_Health>().max_health;
+		healthBar.value = joueur.GetComponent<Has_Health>().health;
 	}
 
 	protected override void onProcess(int familiesUpdateCount)
@@ -30,7 +37,8 @@ public class Attack_J_System : FSystem {
 				aj.has_attacked = true;
 				joueur.GetComponent<Has_Health>().health -= aj.strength;
 			}
-
 		}
+		healthBar.value = joueur.GetComponent<Has_Health>().health;
+
 	}
 }
