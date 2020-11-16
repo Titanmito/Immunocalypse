82616a82ee442ea7f6f76ce9063751b4090d912f
arianacarnielli <ariana.carnielli@gmail.com>
2020-11-16 19:09:12 +0100

    petite modification sur la création des ennemis

--- Immunocalypse/Assets/Systems/Spawn_System.cs
+++ Immunocalypse/Assets/Systems/Spawn_System.cs
@@ -3,6 +3,7 @@ using FYFY;
 using UnityEngine.UI;
 using System.Reflection;
 using System;
+using System.Collections.Generic;
 
 
 public class Spawn_System : FSystem {
@@ -10,11 +11,21 @@ public class Spawn_System : FSystem {
 
 	private Family _Spawn = FamilyManager.getFamily(new AllOfComponents(typeof(Spawn)));
 	private Family _Buttons = FamilyManager.getFamily(new AnyOfTags("Button"));
+	private Family _Spawn_nb = FamilyManager.getFamily(new AnyOfTags("Wave"));
 	private Spawn spawn;
+	private Text spawn_nb;
+
+	private GameObject[] enemies = new GameObject[2];
 
 	public Spawn_System()
 	{
 		spawn = _Spawn.First().GetComponent<Spawn>();
+		spawn_nb = _Spawn_nb.First().GetComponent<Text>();
+
+		// To facilitate the creation of enemies in onProcess.
+		// I couldn't find an automated way of creating this array.
+		enemies[0] = spawn.virus_prefab;
+		enemies[1] = spawn.bacterie_prefab;
 
 		// Puts the price of each tower and special power in their respective buttons.
 		// buttons must be named as *same start as the name of the respective prefab*_button.
@@ -30,33 +41,28 @@ public class Spawn_System : FSystem {
 		}
 	}
 
-	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
 		spawn.spawn_prog += Time.deltaTime;
 
 		if (spawn.spawn_prog >= spawn.wait_time && spawn.nb_waves > 0)
 		{
-			// nb_enemies[0] -> virus
-			// nb_enemies[1] -> bacterie
-			for (int i = 0; i < spawn.nb_enemies[0]; i++)
+			// For each type of enemy we go thru the creation process.
+            for (int j = 0; j < enemies.Length; j++)
             {
-				GameObject go = UnityEngine.Object.Instantiate<GameObject>(spawn.virus_prefab);
-				GameObjectManager.bind(go);
-
-				go.GetComponent<Can_Move>().spawn_point = new Vector3(go.GetComponent<Can_Move>().spawn_point.x, UnityEngine.Random.Range(-1.0f, 1.0f));
-			}
-
-			for (int i = 0; i < spawn.nb_enemies[1]; i++)
-			{
-				GameObject go = UnityEngine.Object.Instantiate<GameObject>(spawn.bacterie_prefab);
-				GameObjectManager.bind(go);
-
-				go.GetComponent<Can_Move>().spawn_point = new Vector3(go.GetComponent<Can_Move>().spawn_point.x, UnityEngine.Random.Range(-1.0f, 1.0f));
+				// We create as many of an type of enemy as the correspondant number in nb_enemies.
+				for (int i = 0; i < spawn.nb_enemies[j]; i++)
+				{
+					GameObject go = UnityEngine.Object.Instantiate<GameObject>(enemies[j]);
+					GameObjectManager.bind(go);
+					go.GetComponent<Can_Move>().spawn_point = new Vector3(go.GetComponent<Can_Move>().spawn_point.x, UnityEngine.Random.Range(-1.0f, 1.0f));
+				}
 			}
 
 			spawn.spawn_prog = 0;
 			spawn.nb_waves--;
 		}
 
+		spawn_nb.text = "wave: " + spawn.nb_waves.ToString();
+		//spawn_nb.color = new Color(0.0f, 0.57f, 0.06f, 1f);
 	}
 }
