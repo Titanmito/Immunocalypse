afed7d0126151a3cfd5c9a66428ff8247f31d543
arianacarnielli <ariana.carnielli@gmail.com>
2020-11-17 20:32:42 +0100

    petite animation pour les tours

---
+++ Immunocalypse/Assets/AutomaticScript/Tower_Animation_System_wrapper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using FYFY;
+
+[ExecuteInEditMode]
+public class Tower_Animation_System_wrapper : MonoBehaviour
+{
+	private void Start()
+	{
+		this.hideFlags = HideFlags.HideInInspector; // Hide this component in Inspector
+	}
+
+}
---
+++ Immunocalypse/Assets/Systems/Tower_Animation_System.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using FYFY;
+using System.Collections;
+
+public class Tower_Animation_System : FSystem {
+
+    private Family _TowerGO = FamilyManager.getFamily(new AnyOfTags("Tower"));
+
+    public Tower_Animation_System()
+    {
+		MainLoop.instance.StartCoroutine(animation_coroutine());
+    }
+
+	private IEnumerator animation_coroutine()
+    {
+        Vector3 scaleChange = new Vector3(0.005f, 0.005f, 0.005f);
+
+        int i = 0;
+        int j = 0;
+		for( ; ; )
+        {
+            foreach (GameObject go in _TowerGO)
+            {
+                if (j % 2 == 0)
+                {
+                    go.transform.localScale += scaleChange;
+                }
+                else
+                {
+                    go.transform.localScale -= scaleChange;
+                }
+                j += 1;
+            }
+            j = 0;
+            i += 1;
+            if (i >= 6)
+            {
+                scaleChange *= -1;
+            }
+
+            i = i % 6;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
