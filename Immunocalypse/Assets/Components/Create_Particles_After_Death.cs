f241d6486da1563f95307d0c410570456fcd8bef
iKachaykin <ivankachaykin707@gmail.com>
2020-11-18 22:56:10 +0200

    Animation d'une mort d'un ennemi
    Maintenant à chaque fois quand un ennemi meurt le système de destruction crée à sa place des petites particules qui se diffusent sur la carte

---
+++ Immunocalypse/Assets/Components/Create_Particles_After_Death.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Create_Particles_After_Death : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+    // The number of particles to create after death
+    public int particles_number;
+
+    // Prefab of particles
+    public GameObject particles_prefab;
+
+    // Force that would be used to the particle when it would be created
+    public float explosion_force = 100f;
+
+}
--- Immunocalypse/Assets/Systems/Destruction_System.cs
+++ Immunocalypse/Assets/Systems/Destruction_System.cs
@@ -7,18 +7,34 @@ public class Destruction_System : FSystem {
 
 	private Family _EnemiesAliveGO = FamilyManager.getFamily(new AnyOfTags("Respawn"));
 	private Family _AlliesAliveGO = FamilyManager.getFamily(new AnyOfTags("Tower"), new AllOfComponents(typeof(Has_Health)), new AllOfComponents(typeof(Lifespan)));
+    private Family _ParticlesAliveGO = FamilyManager.getFamily(new AnyOfTags("Particle"), new AllOfComponents(typeof(Lifespan)));
+    private float _DestroyParticlesProgress = 0.0f, _DestroyParticlesReload = 0.05f;
 
 	protected override void onProcess(int familiesUpdateCount)
 	{
+        _DestroyParticlesProgress += Time.deltaTime;
 		// destroy enemies that should be destroyed.
 		foreach (GameObject go in _EnemiesAliveGO)
 		{
 			Has_Health hh = go.GetComponent<Has_Health>();
 			Attack_J aj = go.GetComponent<Attack_J>();
+            Create_Particles_After_Death cpad = go.GetComponent<Create_Particles_After_Death>();
 
 			// it destroys all enemies with negative or equal to zero energy and those who have already attacked (and aren't visible anymore).
 			if (hh.health <= 0 || aj.has_attacked)
 			{
+                // If an enemy has a component Create_Particles_After_Death it means that we must create particles in the place where it dies
+                if (cpad != null)
+                {
+                    for (int i = 0; i < cpad.particles_number; i++)
+                    {
+                        GameObject particle = UnityEngine.Object.Instantiate<GameObject>(cpad.particles_prefab, go.transform.position, go.transform.rotation);
+                        GameObjectManager.bind(particle);
+                        particle.GetComponent<SpriteRenderer>().color = go.GetComponent<SpriteRenderer>().color;
+                        Rigidbody2D prb = particle.GetComponent<Rigidbody2D>();
+                        prb.AddForce(new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * cpad.explosion_force);
+                    }
+                }
 				GameObjectManager.unbind(go);
 				Object.Destroy(go);
 			}
@@ -40,7 +56,27 @@ public class Destruction_System : FSystem {
 				ls.lifespan -= Time.deltaTime;
 			}
 
-		}
+        }
 
-	}
+        // This loop destroys all the particles after they live their lifespan
+        // ATTENTION This loop would probably force a RETURN OF THE METHOD that is why it is necessary to put it at the very end of this method !!!
+        foreach (GameObject go in _ParticlesAliveGO)
+        {
+            Lifespan ls = go.GetComponent<Lifespan>();
+
+            if (ls.lifespan <= 0 && _DestroyParticlesProgress >= _DestroyParticlesReload)
+            {
+                GameObjectManager.unbind(go);
+                Object.Destroy(go);
+                _DestroyParticlesProgress = 0f;
+                return;
+            }
+            else
+            {
+                ls.lifespan -= Time.deltaTime;
+            }
+
+        }
+
+    }
 }
